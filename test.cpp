eb287b0c09aa78d1b2cf33ebd01e81942945a241
passby6someone <945879305@qq.com>
2019-09-20 23:53:58 +0800

    5

--- test.cpp
+++ test.cpp
@@ -1,10 +1,24 @@
-// #include"ListInSequence.h"
+/*
+*说实话我是很纠结的
+*函数到底写到成员函数里去还是全局声明
+*函数模板到底要不要定义返回值
+*类要不要写函数模板
+*超级纠结，关键是我c++还不熟练，lambad啊，并发啊，接口啊很多都不会写
+*一边写一边纠结，但是书上一些奇妙的设定让我觉得还是写到成员函数里比较好
+*比如insfirst，因为插入了数据，但是由于传入参数的局限，并不能对链表的长度进行修改
+*但是如果我写到成员函数里去，就可以修改其成员变量
+*但是这样又有个问题，我要参数何用？反正成员函数可以修改成员变量。。。
+*感觉就是很zz。。。
+*最后还是决定写成员函数并按照书上的要传的参数来好了
+*btw模板真nm难用
+*/
 #include<stdlib.h>
 #include<stdio.h>
 #include<iostream>
 
 using namespace std;
 
+//节点类模板
 template <typename ElemType>
 class LNode {
     public:
@@ -12,36 +26,62 @@ class LNode {
         LNode *next;
         null();
 };
+//节点类模板构造函数，用于置空指针
 template <typename ElemType>
 LNode<ElemType>::null(){
     this.next=NULL;
 };
 
+
+//链表表头类模板
 template <typename ElemType>
 class LinkList{
     public:
         int len;
         LNode<ElemType> *head,*tail;
-        //just a demo
-        // template <typename Status>
-        // Status mdzz(ElemType);
-        InitList();
+        LinkList();
+        // ~ReleaseList (void) {cout<<"nmsl";}
+        ~LinkList();
         template <typename Status>
         Status ClearList(LinkList<ElemType> &L);
+        template <typename Status>
+        Status DestoryList(LinkList<ElemType> *&L);
+        template <typename Status>
+        Status InsFirst(LinkList<ElemType> *h,LNode<ElemType> *s);
+        // int mdzz(){cout<<"nmsl";};
 };
+//链表表头类模板的构造函数，用于初始化长度和置空指针
 template <typename ElemType>
-LinkList<ElemType>::InitList(){
-    this.len=0;
-    this.head=NULL;
-    this.tail=this.head;
+LinkList<ElemType>::LinkList(){
+    this->len=0;
+    this->head=NULL;
+    this->tail=this->head;
 };
-
-template <typename Status,typename ElemType>
-Status DestoryList(LinkList<ElemType> *&L){
+//链表表头类模板的析构函数，用于在删除表头时检查指向节点空间是否释放
+template <typename ElemType>
+LinkList<ElemType>::~LinkList(){
+    if(this->head!=NULL){
+        LNode<ElemType> *tmp=NULL;
+        while(this->tail!=this->head){
+            tmp=this->head;
+            this->head=this->head->next;
+            delete tmp;
+            tmp=NULL;
+        }
+        delete this->head;
+        this->tail=this->head=NULL;
+        this->len=0;
+    }
+    
+};
+//删除链表表头
+template <typename ElemType>
+template <typename Status>
+Status LinkList<ElemType>::DestoryList(LinkList<ElemType> *&L){
     delete L;
+    L=NULL;
 }
-
-
+//清楚链表
 template <typename ElemType>
 template <typename Status>
 Status LinkList<ElemType>::ClearList(LinkList<ElemType> &L){
@@ -54,13 +94,12 @@ Status LinkList<ElemType>::ClearList(LinkList<ElemType> &L){
     }
     delete L.head;
     L.tail=L.head=NULL;
+    L.len=0;
 }
-
-
-
-
-template <typename Status,typename ElemType>
-Status InsFirst(LinkList<ElemType> *h,LNode<ElemType> *s){
+//表头插入
+template <typename ElemType>
+template <typename Status>
+Status LinkList<ElemType>::InsFirst(LinkList<ElemType> *h,LNode<ElemType> *s){
     if(h->head!=NULL){
         LNode<ElemType> *node=h->head;
         h->head=s;
@@ -71,6 +110,8 @@ Status InsFirst(LinkList<ElemType> *h,LNode<ElemType> *s){
         h->tail=s;
     }
 }
+
+
 template <typename Status,typename ElemType>
 Status Append(LinkList<ElemType> L,LNode<ElemType> s){
     
@@ -85,10 +126,10 @@ int main(){
     LNode<int> *node=new LNode<int>;
     node->data=23;
     // cout<<InitList<int>(List)<<endl;
-    InsFirst<void,int>(List,node);
-    InsFirst<void,int>(List,node);
+    List->InsFirst<void>(List,node);
+    List->InsFirst<void>(List,node);
     List->ClearList<void>(*List);
-    DestoryList<void,int>(List);
+    List->DestoryList<void>(List);
     cin>>i;
     return 0;
 }
